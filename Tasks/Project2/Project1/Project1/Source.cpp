3c063f3c30be61388085694c4299496657b4a456
Anton <super.tosh2016@yandex.by>
2021-12-22 13:31:35 +0300

    T5

---
+++ Tasks/Project2/Project1/Project1/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	    int n;
+		cout << "Enter desired number: ";
+		cin >> n;
+		while (n)
+		{
+			cout << n % 10 << endl;
+			n /= 10;
+		}
+		system("pause");
+	}
